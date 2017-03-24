2f44e745b35277103a39bd9f228503ca79c93b98
Mihir Shete <mihirsht@gmail.com>
2017-03-24 12:33:08 -0500

    add module to monitor memory

---
+++ src/modules/memory.cpp
@@ -0,0 +1,33 @@
+#include <fstream>
+#include <module.h>
+#include <spdlog/spdlog.h>
+#include <sstream>
+
+using namespace kronos::module;
+
+auto memlogger = spdlog::stdout_color_mt("memtask");
+
+class MemoryTask : public PersistModule {
+    public:
+        MemoryTask() : PersistModule("memtask") {};
+        float run() {
+            std::ifstream statf("/proc/self/statm");
+            if (statf.is_open()) {
+                std::string line;
+                std::getline(statf, line);
+                std::istringstream memline(line);
+                int mem;
+
+                memline >> mem;;
+                memlogger->info("memory: {}", mem);
+                statf.close();
+                return mem;
+            }
+            memlogger->error("unable to open file");
+            return -1;
+        }
+        
+        ~MemoryTask(){}
+};
+
+REGISTER(MemoryTask,5);
